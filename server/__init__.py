b22f2728a8d3b8e3c6784b83daa98ed349dfad1e
tatavoSenna <luiz.senna@parafernalia.net.br>
2020-05-24 16:54:56 -0300

    Adjust to run with gunicorn

--- server/__init__.py
+++ server/__init__.py
@@ -5,6 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, MigrateCommand
 from flask_marshmallow import Marshmallow
 
+
 application = Flask(__name__)
 application.debug = True
 application.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
@@ -14,4 +15,16 @@ CORS(application)
 db = SQLAlchemy(application)
 migrate = Migrate(application, db)
 ma = Marshmallow(application)
+
+from app.documents.blueprint import documents_api
+from app.docusign.blueprint import docusign_api
+from app.auth.blueprint import auth_api
+
+application.register_blueprint(documents_api, url_prefix='/documents')
+application.register_blueprint(docusign_api, url_prefix='/docusign')
+application.register_blueprint(auth_api, url_prefix='/auth')
+
+@application.route('/', methods=['GET'])
+def welcome():
+    return 'Welcome do Doing.law API'
     
