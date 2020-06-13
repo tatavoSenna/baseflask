c423f0bcc9689dbb5ab2d0d5c6b7eb4736e2703e
tatavoSenna <luiz.senna@parafernalia.net.br>
2020-06-13 09:02:25 -0300

    Create aws docker login bash script

--- devops/aws_repository_login.sh
+++ devops/aws_repository_login.sh
@@ -1,5 +1,3 @@
 
-aws s3 cp front/build/ s3://lawing-app --profile lawing --recursive
-
-
+#! bin/sh
 aws ecr get-login-password --profile lawing | docker login --username AWS --password-stdin 833120502863.dkr.ecr.us-east-1.amazonaws.com/lawing-api
