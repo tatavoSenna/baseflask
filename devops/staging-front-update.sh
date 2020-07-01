d40c5fa731583f44c33996e1c0b196ae4af602c3
tatavoSenna <luiz.senna@parafernalia.net.br>
2020-06-30 23:00:54 -0300

    Add publicread acl when loading front

--- devops/staging-front-update.sh
+++ devops/staging-front-update.sh
@@ -4,5 +4,5 @@ cd front
 export NODE_ENV=staging
 yarn build
 cd ..
-aws s3 cp front/build/ s3://lawing-app --profile lawing --recursive
+aws s3 cp front/build/ s3://lawing-app --profile lawing --recursive --acl public-read
 aws cloudfront create-invalidation --distribution-id E2YHPG9IX2A98K --paths "/*" --profile lawing
