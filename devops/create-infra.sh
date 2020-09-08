8b35ca3c8be58c1477eda1ff132a2916064ef11c
tatavoSenna <luiz.senna@parafernalia.net.br>
2020-09-08 07:24:34 -0300

    Create dev environment

--- devops/create-infra.sh
+++ devops/create-infra.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
-aws s3 cp devops/cf-templates/ s3://lawing-cognito-cloudformation --profile lawing-cognito --recursive
+aws s3 cp devops/cf-templates/ s3://lawing-cloudformation-template --profile lawing --recursive --acl public-read
 aws cloudformation create-stack \
-    --stack-name cognito \
-    --template-url https://lawing-cognito-cloudformation.s3.amazonaws.com/lawing-master-template.yaml \
+    --stack-name dev \
+    --template-url https://lawing-cloudformation-template.s3.amazonaws.com/lawing-master-template.yaml \
     --parameter ParameterKey=DBName,ParameterValue=testdb \
         ParameterKey=DBUser,ParameterValue=lawingdbuser \
         ParameterKey=DBPassword,ParameterValue=98798fhe8G8hG5 \
         ParameterKey=GitHubToken,ParameterValue=3f6e104464238d4f5ebffe8d5278f466b36494fa \
-        ParameterKey=AppDomainName,ParameterValue=cognito.lawing.com.br \
-        ParameterKey=TemplateBucket,ParameterValue=lawing-cognito-cloudformation \
-        ParameterKey=GitHubBranch,ParameterValue=cognito\
+        ParameterKey=AppDomainName,ParameterValue=dev.lawing.com.br \
+        ParameterKey=TemplateBucket,ParameterValue=lawing-cloudformation-template \
+        ParameterKey=GitHubBranch,ParameterValue=dev\
     --capabilities CAPABILITY_IAM \
-    --profile lawing-cognito
+    --profile lawing    
--- devops/update-infra.sh
+++ devops/update-infra.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
-aws s3 cp devops/cf-templates/ s3://lawing-cognito-cloudformation --profile lawing-cognito --recursive
+aws s3 cp devops/cf-templates/ s3://lawing-cloudformation-template --profile lawing --recursive --acl public-read
 aws cloudformation update-stack \
     --stack-name \
-        cognito \
+        dev \
     --template-url \
-        https://lawing-cognito-cloudformation.s3.amazonaws.com/lawing-master-template.yaml \
+        https://lawing-cloudformation-template.s3.amazonaws.com/lawing-master-template.yaml \
     --parameter \
         ParameterKey=GitHubToken,ParameterValue=3f6e104464238d4f5ebffe8d5278f466b36494fa \
-        ParameterKey=AppDomainName,ParameterValue=cognito.lawing.com.br \
+        ParameterKey=AppDomainName,ParameterValue=dev.lawing.com.br \
         ParameterKey=DBUser,ParameterValue=lawingdbuser ParameterKey=DBPassword,ParameterValue=hHgTf54Rf \
-        ParameterKey=TemplateBucket,ParameterValue=lawing-cognito-cloudformation \
-        ParameterKey=GitHubBranch,ParameterValue=cognito \
+        ParameterKey=TemplateBucket,ParameterValue=lawing-cloudformation-template \
+        ParameterKey=GitHubBranch,ParameterValue=dev \
     --profile \
-        lawing-cognito
+        lawing
