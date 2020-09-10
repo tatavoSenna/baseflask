62e5fd7bb2df019ecaf090609fff9a94f4bb12ae
tatavoSenna <luiz.senna@parafernalia.net.br>
2020-09-10 14:00:55 -0300

    Adjust mispeled react env var in logout

--- front/src/services/AdminRoutes.js
+++ front/src/services/AdminRoutes.js
@@ -11,7 +11,7 @@ export default function RouteWrapper({
 	const { signed } = store.getState().session
 
 	if (!signed && isPrivate) {
-		window.location.replace(process.env.REACT_APP_API_SIGN_URL, '_self')
+		window.location.replace(process.env.REACT_APP_API_SIGN_IN_URL, '_self')
 	}
 
 	if (signed && !isPrivate) {
