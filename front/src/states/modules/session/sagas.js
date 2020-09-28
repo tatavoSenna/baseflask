110972c0695bb372d5fb2f165e7a7f2afb524e8e
Ian <ian.collares@parafernalia.net.br>
2020-09-28 19:27:40 -0300

    Fix logout and redirect

--- front/src/states/modules/session/sagas.js
+++ front/src/states/modules/session/sagas.js
@@ -37,7 +37,7 @@ function* getTokenSaga({ payload }) {
 function* logoutSaga({ payload }) {
 	try {
 		yield put(logoutSuccess())
-		// window.location.replace(process.env.REACT_APP_API_SIGN_IN_URL)
+		window.location.replace(process.env.REACT_APP_API_SIGN_IN_URL)
 	} catch (error) {
 		yield put(logoutFailure(error))
 	}
