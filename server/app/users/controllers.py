782403626653b4566fb0fc1eb530ccb336b7020e
Ian <ian.collares@parafernalia.net.br>
2020-09-30 18:49:02 -0300

    Create Group CRUD

--- server/app/users/controllers.py
+++ server/app/users/controllers.py
@@ -1,4 +1,5 @@
-from app.models.user import User
+from app import db
+from app.models.user import User, UserGroup
 from sqlalchemy import or_
 from flask import jsonify
 from app.serializers.user_serializers import UserSerializer
@@ -15,3 +16,24 @@ def list_user_controller(logged_user, page=1, per_page=20, search_param=''):
     )
 
     return paginated_query
+
+
+def list_user_group_controller(logged_user, page=1, per_page=20, search_param=''):
+    user_groups = UserGroup.query.filter_by(
+        company_id=logged_user.get("company_id", -1), active=True
+    )
+
+    return user_groups
+
+
+def create_user_group_controller(name, company_id):
+    new_user_group = UserGroup(name=name, company_id=company_id)
+    db.session.add(new_user_group)
+    db.session.commit()
+    return new_user_group
+
+
+def delete_user_group_controller(user_group):
+    user_group.active = False
+    db.session.add(user_group)
+    db.session.commit()
