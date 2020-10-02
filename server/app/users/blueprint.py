2a56a5462512fa057ef1d917bff05f974a379d6b
Ian <ian.collares@parafernalia.net.br>
2020-10-02 18:13:24 -0300

    Add Group Management API

--- server/app/users/blueprint.py
+++ server/app/users/blueprint.py
@@ -17,7 +17,10 @@ from .controllers import (
     list_group_controller,
     create_group_controller,
     delete_group_controller,
-    get_group_controller
+    get_group_controller,
+    add_user_to_group_controller,
+    list_users_on_group_controller,
+    remove_user_from_group_controller,
 )
 
 users_bp = Blueprint("users", __name__)
@@ -218,5 +221,102 @@ def delete_group(logged_user, group_id):
         return dict(error="User Group not found"), 404
 
     delete_group_controller(group)
+
+    return {}, 204
+
+
+@groups_bp.route("<group_id>/users", methods=["GET"])
+@aws_auth.authentication_required
+@get_local_user
+def list_users_on_group(logged_user, group_id):
+    try:
+        page = int(request.args.get(
+            "page", current_app.config['PAGE_DEFAULT']))
+        per_page = int(request.args.get(
+            "per_page", current_app.config['PER_PAGE_DEFAULT']))
+        search_param = str(request.args.get("search", ""))
+    except:
+        return {}, 400
+
+    company_id = logged_user['company_id']
+    group = Group.query.get(group_id)
+    if not group:
+        return {}, 404
+
+    if group.company_id != company_id:
+        return {}, 400
+
+    paginated_query = list_users_on_group_controller(
+        group.id, page, per_page, search_param)
+
+    return jsonify(
+        {
+            "page": paginated_query.page,
+            "per_page": paginated_query.per_page,
+            "total": paginated_query.total,
+            "users": UserSerializer(many=True).dump(paginated_query.items),
+        }
+    )
+
+
+@ groups_bp.route("<group_id>/users", methods=["POST"])
+@aws_auth.authentication_required
+@get_local_user
+def add_user_to_group(logged_user, group_id):
+    fields = request.get_json()
+    required_fields = ["user_id"]
+
+    if not all(f in fields for f in required_fields):
+        return dict(error="Missing required fields"), 400
+
+    company = Company.query.get(logged_user['company_id'])
+    if not company:
+        return dict(error="Couldn't find any company for the current user"), 404
+
+    group = Group.query.get(group_id)
+    if not group:
+        return dict(error="Couldn't find group"), 404
+
+    if group.company_id != logged_user['company_id']:
+        return dict(error="Logged user has no access to specified group"), 400
+
+    user_id = fields.get("user_id")
+    user = User.query.get(user_id)
+    if not user:
+        return dict(error="Couldn't find specified user"), 404
+
+    if user.company_id != logged_user['company_id']:
+        return dict(error="Specified user doesn't belong to your company"), 400
+
+    add_user_to_group_controller(
+        group_id=group_id, user_id=logged_user['id'])
+
+    return {}, 204
+
+
+@ groups_bp.route("<group_id>/users/<user_id>", methods=["DELETE"])
+@aws_auth.authentication_required
+@get_local_user
+def remove_user_from_group(logged_user, group_id, user_id):
+
+    company = Company.query.get(logged_user['company_id'])
+    if not company:
+        return dict(error="Couldn't find any company for the current user"), 404
+
+    group = Group.query.get(group_id)
+    if not group:
+        return dict(error="Couldn't find group"), 404
+
+    if group.company_id != logged_user['company_id']:
+        return dict(error="Logged user has no access to specified group"), 400
+
+    user = User.query.get(user_id)
+    if not user:
+        return dict(error="Couldn't find specified user"), 404
+
+    if user.company_id != logged_user['company_id']:
+        return dict(error="Specified user doesn't belong to your company"), 400
+
+    remove_user_from_group_controller(group_id, user_id)
 
     return {}, 204
--- server/app/users/controllers.py
+++ server/app/users/controllers.py
@@ -1,6 +1,6 @@
 from app import db
 from sqlalchemy import or_
-from app.models.user import User, Group
+from app.models.user import User, Group, ParticipatesOn
 from flask import jsonify
 from app.serializers.user_serializers import UserSerializer
 
@@ -48,4 +48,37 @@ def get_group_controller(company_id, group_id):
 def delete_group_controller(group):
     group.active = False
     db.session.add(group)
+    db.session.commit()
+
+
+def add_user_to_group_controller(group_id, user_id):
+    new_participation = ParticipatesOn(group_id=group_id, user_id=user_id)
+    db.session.add(new_participation)
+    db.session.commit()
+
+
+def list_users_on_group_controller(group_id, page=1, per_page=20, search_param=""):
+
+    participations = ParticipatesOn.query.filter_by(group_id=group_id).all()
+    user_id_list = list(map(
+        lambda participation: participation.user_id, participations
+    ))
+
+    paginated_query = (
+        User.query
+        .filter(User.id.in_(user_id_list))
+        .filter(or_(User.name.ilike(f"%{search_param}%"),
+                    User.surname.ilike(f"%{search_param}%"),
+                    User.email.ilike(f"%{search_param}%")))
+        .order_by(User.name)
+        .paginate(page=page, per_page=per_page)
+    )
+
+    return paginated_query
+
+
+def remove_user_from_group_controller(group_id, user_id):
+    participation = ParticipatesOn.query.filter_by(
+        group_id=group_id, user_id=user_id).first()
+    db.session.delete(participation)
     db.session.commit()
