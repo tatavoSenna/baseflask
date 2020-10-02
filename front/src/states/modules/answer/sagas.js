08873919b8bae0dd4c043b88b5be2e4b72f38e84
Ian <ian.collares@parafernalia.net.br>
2020-10-02 08:31:34 -0300

    Add message feedback on document creation

--- front/src/states/modules/answer/sagas.js
+++ front/src/states/modules/answer/sagas.js
@@ -1,6 +1,12 @@
 import { call, put, takeEvery, select } from 'redux-saga/effects'
 
 import api from '~/services/api'
+import {
+	loadingMessage,
+	successMessage,
+	errorMessage,
+} from '~/services/messager'
+
 import { answerRequest, answerSuccess, answerFailure } from '.'
 
 export default function* rootSaga() {
@@ -9,6 +15,9 @@ export default function* rootSaga() {
 
 function* answerSaga({ payload }) {
 	const { history } = payload
+
+	loadingMessage({ content: 'Criando documento...', updateKey: 'answer' })
+
 	const { answer, modelId } = yield select((state) => {
 		const {
 			answer,
@@ -23,8 +32,16 @@ function* answerSaga({ payload }) {
 		})
 
 		yield put(answerSuccess(data))
+		successMessage({
+			content: 'Documento criado com sucesso!',
+			updateKey: 'answer',
+		})
 		history.push('/')
 	} catch (error) {
 		yield put(answerFailure(error))
+		errorMessage({
+			content: 'A criação do documento falhou',
+			updateKey: 'answer',
+		})
 	}
 }
