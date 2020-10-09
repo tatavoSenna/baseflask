273ac375f0f8ce22839ffad1d3a76dde5b1dd243
André Cordeiro <andrecordeiro.oliveira@hotmail.com>
2020-10-09 20:19:43 +0000

    [Feature/106] Convert the Current Design of the Documents list to a table, instead of cards

---
+++ front/src/components/dataTable/index.js
@@ -0,0 +1,89 @@
+import React, { useState } from 'react'
+import { string, shape, arrayOf, array, number, func, bool } from 'prop-types'
+import { Table, Input, Button, Empty } from 'antd'
+
+const { Search } = Input
+
+const DataTable = ({
+	columns,
+	dataSource,
+	pages,
+	onChangePageNumber,
+	placeholderSearch,
+	textButton,
+	onSearch,
+	onClickButton,
+	placeholderNoData,
+	loading,
+}) => {
+	const [search, setSearch] = useState('')
+	return (
+		<>
+			<div
+				className="searchbox"
+				style={{
+					display: 'flex',
+					justifyContent: 'flex-end',
+					gap: '5px',
+					margin: '24px 0',
+				}}>
+				<Search
+					placeholder={placeholderSearch}
+					onSearch={(search) => onSearch({ page: 1, perPage: 10, search })}
+					onChange={(e) => setSearch(e.target.value)}
+					enterButton
+					style={{ width: '50%' }}
+				/>
+				<Button onClick={onClickButton}>{textButton}</Button>
+			</div>
+			<Table
+				columns={columns}
+				dataSource={dataSource}
+				pagination={{
+					showSizeChanger: true,
+					pageSize: pages.per_page,
+					position: ['bottomCenter'],
+					total: pages.total,
+					current: pages.page,
+					onShowSizeChange: (page, perPage) =>
+						onChangePageNumber({ page, perPage, search }),
+					onChange: (page, perPage) =>
+						onChangePageNumber({ page, perPage, search }),
+					locale: { items_per_page: '' },
+				}}
+				locale={{ emptyText: <Empty description={placeholderNoData} /> }}
+				loading={loading}
+			/>
+		</>
+	)
+}
+
+DataTable.propTypes = {
+	columns: arrayOf(
+		shape({
+			title: string,
+			dataIndex: string,
+		})
+	).isRequired,
+	dataSource: array.isRequired,
+	pages: shape({
+		per_page: number,
+		page: number,
+		total: number,
+	}).isRequired,
+	onChangePageNumber: func.isRequired,
+	placeholderSearch: string,
+	textButton: string.isRequired,
+	onSearch: func.isRequired,
+	onClickButton: func.isRequired,
+	placeholderNoData: string,
+	loading: bool,
+}
+
+DataTable.defaultProps = {
+	placeholderSearch: 'Search',
+	placeholderNoData: 'Nenhum dado encontrado',
+	loading: false,
+}
+
+export default DataTable
