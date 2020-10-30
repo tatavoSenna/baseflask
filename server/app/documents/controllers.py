076bd649896731ac03c9b52a3da836814be3ac88
Ian Conde Collares <ian.collares@parafernalia.net.br>
2020-10-30 17:36:50 +0000

    Integrate document creation to s3

--- server/app/documents/controllers.py
+++ server/app/documents/controllers.py
@@ -1,7 +1,11 @@
 import json
+import uuid
+from datetime import datetime
 
 from app import db
+from app.constants import months
 from app.models.documents import Document, DocumentTemplate
+from .remote import RemoteDocument
 from app.serializers.document_serializers import (
     DocumentSerializer
 )
@@ -23,23 +27,44 @@ def get_document_template_details_controller(company_id, template_id):
   
     return document_template
 
-def create_document(
-    company_id,
-    user_id,
-    title,
-    document_template_id,
-):
+def create_document_controller(user_id, company_id, variables, document_template_id):
+    document_template = DocumentTemplate.query.get(document_template_id)
+    title = f"{document_template.name}-{uuid.uuid1()}"
+
+    current_date_dict = get_current_date_dict()
+    variables.update(current_date_dict)
+
     document = Document(
         user_id=user_id,
         company_id=company_id,
-
+        form=document_template.form,
+        workflow=document_template.workflow,
+        signers=document_template.signers,
+        variables=variables,
         title=title,
         document_template_id=document_template_id,
     )
     db.session.add(document)
     db.session.commit()
 
+    remote_document = RemoteDocument()
+    remote_document.create(document)
+
     return document
+
+
+def get_current_date_dict():
+    date = {}
+
+    now = datetime.now()
+    date["day"] = str(now.day).zfill(2)
+    date["month"] = months[now.month - 1]
+    date["year"] = now.year
+    date[
+        "today"
+    ] = f'{ date["day"] }/{ date["month"] }/{ date["year"] }'
+
+    return date
 
 
 def get_document_controller(document_id):
---
+++ server/app/documents/remote.py
@@ -0,0 +1,48 @@
+import boto3
+import io
+
+from datetime import datetime
+from flask import current_app
+
+from app import jinja_env
+
+
+class RemoteDocument:
+
+    s3_client = boto3.client("s3")
+
+    def create(self, document):
+        text_template = self.download_text_from_template(document)
+        filled_text = self.fill_text_with_variables(text_template, document.variables)
+        self.upload_filled_text_to_documents(document, filled_text)
+
+
+    def upload_filled_text_to_documents(self, document, filled_text):
+        remote_path = f'{current_app.config["AWS_S3_DOCUMENTS_ROOT"]}/{document.id}/{document.versions[-1]}.txt'
+        filled_text_io = io.BytesIO(filled_text)
+
+        self.s3_client.upload_fileobj(
+            filled_text_io,
+            current_app.config["AWS_S3_DOCUMENTS_BUCKET"],
+            remote_path
+        )
+
+
+    def download_text_from_template(self, document):
+        text_file_io = io.BytesIO()
+        remote_path = f'{current_app.config["AWS_S3_TEMPLATES_ROOT"]}/{document.document_template_id}.txt'
+
+        self.s3_client.download_fileobj(
+            current_app.config["AWS_S3_DOCUMENTS_BUCKET"],
+            remote_path,
+            text_file_io)
+        text_file = text_file_io.getvalue()
+
+        return text_file
+
+
+    def fill_text_with_variables(self, text_template, variables):
+        jinja_template = jinja_env.from_string(text_template.decode())
+        filled_text = jinja_template.render(variables).encode()
+        
+        return filled_text
