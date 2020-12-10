88b07bd1ec7d307e9d6a6ca072469ac7c0b8723d
João Pedro Nascimento <jpedronas@live.com>
2020-12-10 15:02:23 -0300

    enchanced save keys test

--- server/app/company/tests/docusign_keys_test.py
+++ server/app/company/tests/docusign_keys_test.py
@@ -3,7 +3,7 @@ from app.test import factories
 import pytest
 
 
-def test_add_keys():
+def test_save_keys():
     company = factories.CompanyFactory()
     docusign_integration_key = "34545tgwc45g56vg5ee5"
     docusign_secret_key = "453456574yf4hyv6h767"
@@ -14,4 +14,14 @@ def test_add_keys():
 
     assert company.docusign_integration_key == docusign_integration_key
     assert company.docusign_secret_key == docusign_secret_key
+    assert company.docusign_account_id == docusign_account_id
+
+    docusign_integration_key = ""
+    docusign_secret_key = None
+    docusign_account_id = "67534fty75g6c86h"
+
+    save_company_keys_controller(company, docusign_integration_key, docusign_secret_key, docusign_account_id)
+    
+    assert company.docusign_integration_key == docusign_integration_key
+    assert company.docusign_secret_key == "453456574yf4hyv6h767"
     assert company.docusign_account_id == docusign_account_id
