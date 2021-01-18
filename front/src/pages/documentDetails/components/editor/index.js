f699213ab00ce8312fd88935aa06df0008218d54
André Cordeiro <andrecordeiro.oliveira@hotmail.com>
2021-01-18 19:28:30 +0000

    Feature/124

--- front/src/pages/documentDetails/components/editor/index.js
+++ front/src/pages/documentDetails/components/editor/index.js
@@ -1,11 +1,68 @@
 import React from 'react'
 import CKEditor from '@ckeditor/ckeditor5-react'
-import DecoupledEditor from '@ckeditor/ckeditor5-build-decoupled-document'
+import DecoupledDocumentEditor from 'ckeditor5-custom-build/build/ckeditor'
 import { string, func, bool } from 'prop-types'
 import { classNames } from '~/utils'
 import styles from './index.module.scss'
 import { Form, Button, Spin } from 'antd'
 
+const editorConfiguration = {
+	toolbar: {
+		items: [
+			'exportPdf',
+			'exportWord',
+			'heading',
+			'|',
+			'fontSize',
+			'fontFamily',
+			'|',
+			'bold',
+			'italic',
+			'underline',
+			'strikethrough',
+			'highlight',
+			'|',
+			'alignment',
+			'|',
+			'numberedList',
+			'bulletedList',
+			'|',
+			'indent',
+			'outdent',
+			'|',
+			'todoList',
+			'link',
+			'blockQuote',
+			'imageUpload',
+			'insertTable',
+			'mediaEmbed',
+			'|',
+			'undo',
+			'redo',
+			'comment',
+		],
+	},
+	language: 'pt-br',
+	image: {
+		toolbar: ['imageTextAlternative', 'imageStyle:full', 'imageStyle:side'],
+	},
+	table: {
+		contentToolbar: ['tableColumn', 'tableRow', 'mergeTableCells'],
+	},
+	licenseKey: 'mboC54zxmZJKbEk7qX54q1Do7KHIuLn2fwPhCh4EsmrFlMGa2wi/lfvS',
+	exportPdf: {
+		stylesheets: ['EDITOR_STYLES'],
+		fileName: `documento.pdf`,
+		converterOptions: {
+			format: 'A4',
+			margin_top: '12.7mm',
+			margin_bottom: '12.5mm',
+			margin_right: '12.7mm',
+			margin_left: '12.5mm',
+			page_orientation: 'portrait',
+		},
+	},
+}
 const Editor = ({
 	text,
 	textUpdate,
@@ -46,10 +103,11 @@ const Editor = ({
 								)
 								toolbarContainer.appendChild(editor.ui.view.toolbar.element)
 							}}
-							editor={DecoupledEditor}
-							config={{}}
+							editor={DecoupledDocumentEditor}
+							config={editorConfiguration}
 							onChange={(event, editor) => onUpdateText(editor.getData())}
 							data={!versionLoading ? text : ''}
+							disabled={block || versionLoading}
 						/>
 					</div>
 				</div>
