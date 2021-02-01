fa1910450a9be1d0a489027d42395a53461fb61f
Tatiana Reimer Barata <tatireimer99@gmail.com>
2021-02-01 20:40:44 +0000

    Change create template endpoint to receive template text and save it on S3 | feature-209

--- server/app/templates/controllers.py
+++ server/app/templates/controllers.py
@@ -2,7 +2,9 @@
 from app import db
 from app.models.documents import DocumentTemplate
 
-def create_template_controller(company_id, name, form, workflow, signers):
+from .remote import RemoteTemplate
+
+def create_template_controller(company_id, name, form, workflow, signers, text):
 
     document_template = DocumentTemplate(
         company_id=company_id,
@@ -15,4 +17,8 @@ def create_template_controller(company_id, name, form, workflow, signers):
 
     db.session.add(document_template)
     db.session.commit()
+
+    remote_template = RemoteTemplate()
+    remote_template.upload_template(text, document_template.id)
+
     return document_template.id
