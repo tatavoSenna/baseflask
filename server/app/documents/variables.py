1dbed6b5f96b4cd56185d74e1dd2816d5a88a1a0
tati99 <tatireimer99@gmail.com>
2021-03-26 12:38:03 -0300

    feature-315

--- server/app/documents/variables.py
+++ server/app/documents/variables.py
@@ -1,5 +1,7 @@
 from datetime import datetime
 from app.models.documents import DocumentTemplate
+import requests
+import json
 
 def specify_variables(variables, document_template_id):
     variables_specification = DocumentTemplate.query.get(document_template_id).variables
@@ -13,7 +15,10 @@ def specify_variables(variables, document_template_id):
         if variables_specification[variable]["type"] == "date":
             date = datetime.strptime(variables[variable][0:10], "%Y-%m-%d")
             variables[variable] = date.strftime(variables_specification[variable]["doc_display_style"])
+        elif variables_specification[variable]["type"] == "database":
+            response = requests.get(f'https://n66nic57s2.execute-api.us-east-1.amazonaws.com/dev/extract/{variables[variable]}').json()
+            variables = {**variables, **response}
         else:
             pass
-
+ 
     return variables
