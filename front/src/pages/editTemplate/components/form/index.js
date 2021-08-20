71bdb4bacaa9728a4ab05b270663137ae81b84b1
João Pedro Nascimento <jpedronas@live.com>
2021-08-20 16:13:37 +0000

    LAW-33 Allow reorder of form pages

--- front/src/pages/editTemplate/components/form/index.js
+++ front/src/pages/editTemplate/components/form/index.js
@@ -1,60 +1,144 @@
 import React, { useState } from 'react'
 import { useDispatch } from 'react-redux'
-import { array } from 'prop-types'
-import { Menu, Empty } from 'antd'
+import { array, string, func, any } from 'prop-types'
+import { Tabs, Empty, Button } from 'antd'
+import { DndProvider, DragSource, DropTarget } from 'react-dnd'
+import { HTML5Backend } from 'react-dnd-html5-backend'
 import { PlusOutlined } from '@ant-design/icons'
-
 import {
 	editTemplatePageAdd,
 	editTemplatePageRemove,
+	editTemplatePageMove,
 } from '~/states/modules/editTemplate'
-
 import Page from './page'
+
+const { TabPane } = Tabs
+
+const TabNode = (props) => {
+	const { connectDragSource, connectDropTarget, children } = props
+
+	return connectDragSource(connectDropTarget(children))
+}
+
+const cardTarget = {
+	drop(props, monitor) {
+		const dragKey = monitor.getItem().index
+		const hoverKey = props.index
+
+		if (dragKey === hoverKey) {
+			return
+		}
+
+		props.moveTabNode(dragKey, hoverKey)
+		monitor.getItem().index = hoverKey
+	},
+}
+
+const cardSource = {
+	beginDrag(props) {
+		return {
+			id: props.id,
+			index: props.index,
+		}
+	},
+}
+
+const WrapTabNode = DropTarget('DND_NODE', cardTarget, (connect) => ({
+	connectDropTarget: connect.dropTarget(),
+}))(
+	DragSource('DND_NODE', cardSource, (connect, monitor) => ({
+		connectDragSource: connect.dragSource(),
+		isDragging: monitor.isDragging(),
+	}))(TabNode)
+)
+
+const DraggableTabs = (props) => {
+	const dispatch = useDispatch()
+
+	const moveTabNode = (dragKey, hoverKey) => {
+		dispatch(editTemplatePageMove({ from: dragKey, to: hoverKey }))
+	}
+
+	const onChange = (e) => {
+		props.setCurrent(e)
+	}
+
+	const renderTabBar = (props, DefaultTabBar) => (
+		<DefaultTabBar {...props}>
+			{(node) => (
+				<WrapTabNode key={node.key} index={node.key} moveTabNode={moveTabNode}>
+					{node}
+				</WrapTabNode>
+			)}
+		</DefaultTabBar>
+	)
+
+	const { children } = props
+
+	const tabs = []
+	React.Children.forEach(children, (c) => {
+		tabs.push(c)
+	})
+
+	const add = () => {
+		const newPage = {
+			title: 'Nova Página',
+			fields: [],
+		}
+		dispatch(editTemplatePageAdd({ newPage }))
+		props.setCurrent(`${props.data.length}`)
+	}
+
+	return (
+		<DndProvider backend={HTML5Backend}>
+			<Tabs
+				style={{ maxWidth: '40rem' }}
+				onEdit={add}
+				onChange={onChange}
+				activeKey={props.current}
+				renderTabBar={renderTabBar}
+				{...props}
+				tabBarExtraContent={
+					<Button
+						onClick={() => add()}
+						icon={<PlusOutlined />}
+						style={{
+							border: 'none',
+							position: 'relative',
+							left: '-5px',
+							top: '1px',
+						}}
+					/>
+				}></Tabs>
+		</DndProvider>
+	)
+}
 
 const TemplateForm = ({ data }) => {
 	const dispatch = useDispatch()
 	const [current, setCurrent] = useState('0')
 
-	const handleNav = (e) => {
-		if (e.key === '+') {
-			const newPage = {
-				title: '',
-				fields: [],
-			}
-			dispatch(editTemplatePageAdd({ newPage }))
-			setCurrent(`${data.length}`)
-		} else {
-			setCurrent(`${e.key}`)
-		}
-	}
-
 	const handleRemovePage = (pageIndex) => {
-		dispatch(editTemplatePageRemove({ pageIndex }))
-
-		if (pageIndex === 0) {
-			setCurrent(`${pageIndex}`)
-		} else {
-			setCurrent(`${pageIndex - 1}`)
+		if (pageIndex === data.length - 1) {
+			setCurrent(`${current - 1}`)
 		}
+		dispatch(editTemplatePageRemove({ pageIndex }))
 	}
 	return (
 		<>
-			<Menu onClick={handleNav} mode="horizontal" selectedKeys={[current]}>
+			<DraggableTabs current={current} setCurrent={setCurrent} data={data}>
 				{data.map((page, index) => (
-					<Menu.Item key={`${index}`}>Página {index + 1}</Menu.Item>
+					<TabPane key={index} tab={page.title} closable={false}>
+						<Page
+							pageIndex={index}
+							data={data[index]}
+							handleRemovePage={handleRemovePage}
+						/>
+					</TabPane>
 				))}
-				<Menu.Item key="+">
-					<PlusOutlined />
-				</Menu.Item>
-			</Menu>
-			{!data.length ? (
+			</DraggableTabs>
+			{!data.length && (
 				<Empty description="Sem Páginas" style={{ marginTop: '3rem' }} />
-			) : (
-				<Page
-					pageIndex={parseInt(current)}
-					data={data[current]}
-					handleRemovePage={handleRemovePage}
-				/>
 			)}
 		</>
 	)
@@ -64,4 +148,11 @@ export default TemplateForm
 
 TemplateForm.propTypes = {
 	data: array,
+}
+
+DraggableTabs.propTypes = {
+	current: string,
+	setCurrent: func,
+	data: array,
+	children: any,
 }
