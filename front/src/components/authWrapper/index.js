df46cf551a926eccb5715d5cdf7e1a3ffbd70979
Keller Clayderman <keller.clayderman.m@gmail.com>
2022-01-10 14:26:00 -0300

    LAW-452 - Fix app signup, update amplify component

--- front/src/components/authWrapper/index.js
+++ front/src/components/authWrapper/index.js
@@ -1,13 +1,33 @@
 import React from 'react'
 import { node } from 'prop-types'
-import { AmplifyAuthenticator, AmplifySignIn } from '@aws-amplify/ui-react'
+import { useHistory } from 'react-router-dom'
+import { Authenticator } from '@aws-amplify/ui-react'
+import '@aws-amplify/ui-react/styles.css'
+import { Hub } from 'aws-amplify'
+import LogOutContext from '~/context/LogOutContext'
 
 function AuthWrapper({ children }) {
+	let history = useHistory()
+
+	//TODO: register this listener only once
+	Hub.listen('auth', (data) => {
+		if (data.payload.event === 'signIn') {
+			history.push('/')
+		}
+	})
+
 	return (
-		<AmplifyAuthenticator>
-			<AmplifySignIn slot="sign-in" hideSignUp={true} />
-			{children}
-		</AmplifyAuthenticator>
+		<Authenticator
+			loginMechanisms={['email', 'phone_number']}
+			signUpAttributes={['name', 'email', 'phone_number']}>
+			{({ signOut, user }) => {
+				return (
+					<LogOutContext.Provider value={signOut}>
+						{children}
+					</LogOutContext.Provider>
+				)
+			}}
+		</Authenticator>
 	)
 }
 
