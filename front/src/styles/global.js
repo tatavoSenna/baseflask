ee38b287fdd50217b250a4d28947dc04f71c6edd
Keller Clayderman <keller.clayderman.m@gmail.com>
2022-01-19 12:40:57 -0300

    Change amplify component style to match the rest of the app

--- front/src/styles/global.js
+++ front/src/styles/global.js
@@ -11,11 +11,38 @@ const GlobalStyle = createGlobalStyle`
 	}
 
   :root {
-    --amplify-primary-color: rgb(24, 144, 255);
-    --amplify-primary-tint: #0;
-    --amplify-primary-shade: #0;
 
-    --amplify-font-family: 'Open Sans'
+    --amplify-components-button-primary-border-color: #1890ff;
+    --amplify-components-button-primary-background-color: #1890ff;
+    --amplify-components-button-primary-hover-background-color: #40a9ff;
+    --amplify-components-button-primary-active-background-color: #096dd9;
+    --amplify-components-button-primary-focus-background-color: #40a9ff;
+    --amplify-components-button-primary-focus-box-shadow: none;
+    --amplify-components-button-border-radius: 2px;
+
+    --amplify-components-button-hover-border-color: #40a9ff;
+    --amplify-components-button-hover-background-color: #fafafa;
+    --amplify-components-button-active-border-color: #096dd9;
+    --amplify-components-button-active-background-color: #fafafa;
+    --amplify-components-button-focus-border-color: #40a9ff;
+    --amplify-components-button-focus-background-color: #fafafa;
+    --amplify-components-button-focus-box-shadow: none;
+    
+    --amplify-components-button-link-color: #1890ff;
+    --amplify-components-button-link-hover-color: #40a9ff;
+    --amplify-components-button-link-hover-background-color: #fafafa;
+    --amplify-components-button-link-active-background-color: #e6f7ff;
+    --amplify-components-button-link-focus-background-color: #e6f7ff;
+
+    --amplify-components-tabs-item-hover-color: #1890ff;
+    --amplify-components-tabs-item-active-color: #00404d;
+    --amplify-components-tabs-item-active-border-color: #1890ff;
+
+    --amplify-components-fieldcontrol-border-color: #d9d9d9;
+    --amplify-components-fieldcontrol-border-width: 1px;
+    --amplify-components-fieldcontrol-border-radius: 2px;
+    --amplify-components-fieldcontrol-focus-border-color: #40a9ff;
+    --amplify-components-fieldcontrol-focus-box-shadow: 0 0 0 2px rgb(24 144 255 / 20%);
   }
 
   html {
