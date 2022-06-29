01c456a079de37216ce6dc23c0187a27164e40a2
Luiz Senna <luiz.senna@parafernalia.net.br>
2022-06-28 22:25:30 -0300

    HOTFIX - Adjust formating for black

--- server/app/internal_database/blueprint.py
+++ server/app/internal_database/blueprint.py
@@ -74,7 +74,7 @@ def list_text_items_from_internal_db(current_user, db_id):
         order = str(request.args.get("order", "descend"))
         tag_ids = request.args.get("tag_ids", None)
         if tag_ids:
-            tag_ids = tag_ids.split(',')
+            tag_ids = tag_ids.split(",")
     except Exception as e:
         return BadRequest(description="Malformed parameters")
 
@@ -84,14 +84,7 @@ def list_text_items_from_internal_db(current_user, db_id):
 
     if not text_items_query:
         return (
-            jsonify(
-                {
-                    "page": page,
-                    "per_page": per_page,
-                    "total": 0,
-                    "items": [],
-                }
-            ),
+            jsonify({"page": page, "per_page": per_page, "total": 0, "items": [],}),
             200,
         )
 
