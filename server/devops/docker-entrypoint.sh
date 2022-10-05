3b4a473269cb2304c4889bafc5b468673cf6dafd
Luiz Senna <luiz.senna@parafernalia.net.br>
2022-10-05 20:54:50 -0300

    Enhance production server capacity

--- server/devops/docker-entrypoint.sh
+++ server/devops/docker-entrypoint.sh
@@ -3,6 +3,6 @@ set -e
 exec /bin/sh -c "\
     flask db upgrade --directory /opt/app/migrations; \
     gunicorn wsgi:app \
-        --workers 1 \
+        --workers 4 \
         --bind 0.0.0.0:5000"
 exec "$@";
