04030583a30761420d32fd94c1ad8456a258184c
Luiz Senna <luiz.senna@parafernalia.net.br>
2022-10-20 18:24:17 -0300

    Fix CI and raise memory on ecs containers

--- devops/update-dev-infra.sh
+++ devops/update-dev-infra.sh
@@ -26,6 +26,6 @@ aws cloudformation update-stack \
         ParameterKey=BackEndSentryDSN,UsePreviousValue=true \
         ParameterKey=FrontEndSentryDSN,UsePreviousValue=true \
         ParameterKey=CpuSize,ParameterValue=256 \
-        ParameterKey=MemSize,ParameterValue=512 \
+        ParameterKey=MemSize,ParameterValue=2048 \
     --profile \
         lawing-dev
