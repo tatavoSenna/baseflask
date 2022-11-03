05b5fd3fdb4cb062d1af45f0a67bec76e91bc8a6
Gabriel Carvalho <gabriel.foxcarvalho@gmail.com>
2022-11-03 22:02:07 +0000

    LAW-844 Acrescentando autocomplete no campo de api

--- front/src/components/databaseField/index.js
+++ front/src/components/databaseField/index.js
@@ -1,8 +1,14 @@
-import React, { useEffect, useMemo } from 'react'
-import { string, shape, object, func, bool } from 'prop-types'
-import { apiRequest } from '~/states/modules/databaseField'
-import { useDispatch, useSelector } from 'react-redux'
-import DropdownField from 'components/dropdownField'
+import React, { useEffect, useReducer, useRef, useState } from 'react'
+import { string, shape, object, func, bool, number } from 'prop-types'
+import Axios from 'axios'
+import {
+	errorMessage,
+	INITIAL_STATE,
+	optionsReducer,
+	populateData,
+} from './utils/searchReducer'
+import { ACTION_TYPES } from './utils/searchActionTypes'
+import SearchSelect from './searchSelect'
 
 const DatabaseField = ({
 	pageFieldsData,
@@ -18,29 +24,48 @@ const DatabaseField = ({
 	const search_key = variable?.search_key
 	const display_key = variable?.display_key
 
-	const dispatch = useDispatch()
-	const [data, error] = useSelector(({ databaseField }) => [
-		databaseField.data[url],
-		databaseField.error[url],
-	])
+	const [dataSearch, setDataSearch] = useState('')
+	const waitTimeout = useRef()
+
+	const [state, dispatch] = useReducer(optionsReducer, INITIAL_STATE)
 
 	useEffect(() => {
-		if (url && !data) dispatch(apiRequest({ url }))
-	}, [url, data, dispatch])
+		const cancelToken = Axios.CancelToken.source()
+		if (dataSearch.length >= 3) {
+			dispatch({ type: ACTION_TYPES.FETCH_START })
+			Axios.get(`${url}?search=${dataSearch}`, {
+				cancelToken: cancelToken.token,
+			})
+				.then((response) => {
+					const data = populateData(response, display_key, search_key)
+					dispatch({ type: ACTION_TYPES.FETCH_SUCCESS, payload: data })
+				})
+				.catch((error) => {
+					dispatch({ type: ACTION_TYPES.FETCH_ERROR })
+					errorMessage(Axios, error)
+				})
+		} else {
+			dispatch({ type: ACTION_TYPES.FETCH_CLEAR })
+		}
 
-	const options = useMemo(
-		() =>
-			(data ?? []).map((d) => ({
-				label: String(d[display_key]),
-				value: d[search_key],
-			})),
-		[data, display_key, search_key]
-	)
+		return () => {
+			cancelToken.cancel()
+		}
+	}, [dataSearch, url, search_key, display_key])
+
+	const handleSearch = (e) => {
+		clearTimeout(waitTimeout.current)
+
+		waitTimeout.current = setTimeout(() => {
+			setDataSearch(e)
+		}, 500)
+	}
 
 	return (
-		<DropdownField
-			notFoundContent={error ? 'Falha ao requisitar API externa' : undefined}
-			pageFieldsData={{ ...pageFieldsData, options }}
+		<SearchSelect
+			pageFieldsData={{ ...pageFieldsData, options: state.options }}
+			state={state}
+			onSearch={handleSearch}
 			{...{ className, onChange, inputValue, disabled, visible, form }}
 		/>
 	)
@@ -56,7 +81,7 @@ DatabaseField.propTypes = {
 	form: object,
 	className: string,
 	onChange: func,
-	inputValue: string,
+	inputValue: number,
 	disabled: bool,
 	visible: bool,
 }
