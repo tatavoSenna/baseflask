eb93a7024b5d125255d06525de5ac12d709f1aca
Gabriel Carvalho <gabriel.foxcarvalho@gmail.com>
2022-12-02 12:12:18 +0000

    LAW-CKEditor - adicionando um h3

--- front/src/pages/editTemplate/components/text/editor/index.js
+++ front/src/pages/editTemplate/components/text/editor/index.js
@@ -58,6 +58,18 @@ const Editor = ({ text, onUpdateText, block, getEditor }) => {
 											title: 'Heading 2',
 											class: 'ck-heading_heading2',
 										},
+										{
+											model: 'heading3',
+											view: 'h3',
+											title: 'Heading 3',
+											class: 'ck-heading_heading3',
+										},
+										{
+											model: 'heading4',
+											view: 'h4',
+											title: 'Heading 4',
+											class: 'ck-heading_heading4',
+										},
 									],
 								},
 							}}
